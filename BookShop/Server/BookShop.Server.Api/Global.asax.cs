d5e459d5f608870f89e2f1eb9685152caee5e8c0
IvayloKodov <Ivailo_Kodov@abv.bg>
2017-04-10 16:22:06 +0300

    Added Ninject.Common, extension-conventions

--- BookShop/Server/BookShop.Server.Api/Global.asax.cs
+++ BookShop/Server/BookShop.Server.Api/Global.asax.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace BookShop.Server.Api
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    using System.Reflection;
+    using System.Web;
+    using Common.Mappings;
+
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            NinjectConfig.CreateKernel();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperConfig automapper = new AutoMapperConfig();
+            automapper.Execute(Assembly.GetExecutingAssembly());
         }
     }
 }
